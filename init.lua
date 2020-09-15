d638213eee7f3cce7784e73dbf038232d84ff0e2
Aaron Chan <aaron1@gmail.com>
2020-09-15 15:34:25 -0700

    Updates for multiple apps

--- init.lua
+++ init.lua
@@ -38,35 +38,38 @@ function term_geo(window)
     end
   elseif numScreens == 1 then
     if window:id() == first_term then
-       return hs.geometry.rect(780, 38, 840, 1010)
+       return hs.geometry.rect(700, 38, 740, 850)
     elseif window:id() == second_term then
-       return hs.geometry.rect(1, 60, 780, 985)
+       return hs.geometry.rect(1, 60, 700, 850)
     end
   end
 end
 
-local laptopScreen = "Color LCD"
+-- local laptopScreen = "Color LCD"
+local laptopScreen = hs.screen(69734208)
 local desktopScreen = "DELL U2717D"
 local dualLayout = {
-      {hs.application.find("slack"), nil, desktopScreen, nil, nil, hs.geometry.rect(1515, 0, 1024, 822)},
+      {hs.application.find("slack"), nil, desktopScreen, nil, nil, hs.geometry.rect(1515, 30, 1024, 822)},
+      {hs.application.find("discord"), nil, desktopScreen, nil, nil, hs.geometry.rect(1515, 0, 1024, 822)},
       {hs.application.find("chrome"), nil, desktopScreen, nil, nil, hs.geometry.rect(0, 0, 1620, 1400)},
       {hs.application.find("calculator"), nil, desktopScreen, nil, nil, hs.geometry.rect(2110, 830, 0, 0)},
       {hs.application.find("evernote"), nil, desktopScreen, nil, nil, hs.geometry.rect(1317, 0, 1023, 762)},
       {hs.application.find("iterm2"), nil, desktopScreen, nil, nil, term_geo},
       {hs.application.find("iterm2"), nil, desktopScreen, nil, nil, term_geo},
       {hs.application.find("textedit"), nil, desktopScreen, nil, nil, hs.geometry.rect(1626, 790, 484, 604)},
-      {hs.application.find("firefox"), nil, laptopScreen, nil, nil, hs.geometry.rect(480,1440,1435,900)},
-     -- {hs.application.find("firefox"), nil, laptopScreen, hs.layout.maximized, nil, nil},
+      -- {hs.application.find("firefox"), nil, laptopScreen, nil, nil, hs.geometry.rect(0,0,800,500)}
+      -- {hs.application.find("firefox"), nil, laptopScreen, hs.layout.maximized, nil, nil},
 }
 local singleLayout = {
-      {hs.application.find("slack"), nil, laptopScreen, nil, nil, hs.geometry.rect(700, 0, 980, 822)},
+      {hs.application.find("slack"), nil, laptopScreen, nil, nil, hs.geometry.rect(0, 0, 960, 850)},
+      {hs.application.find("discord"), nil, laptopScreen, nil, nil, hs.geometry.rect(0, 0, 960, 850)},
       {hs.application.find("chrome"), nil, laptopScreen, hs.layout.maximized, nil, nil},
-      {hs.application.find("calculator"), nil, laptopScreen, nil, nil, hs.geometry.rect(1450, 730, 0, 0)},
+      {hs.application.find("calculator"), nil, laptopScreen, nil, nil, hs.geometry.rect(1200, 580, 0, 0)},
       {hs.application.find("evernote"), nil, laptopScreen, nil, nil, hs.geometry.rect(800, 0, 900, 762)},
       {hs.application.find("iterm2"), nil, laptopScreen, nil, nil, term_geo},
       {hs.application.find("iterm2"), nil, laptopScreen, nil, nil, term_geo},
       {hs.application.find("textedit"), nil, laptopScreen, nil, nil, hs.geometry.rect(1200, 400, 4601, 604)},
-      {hs.application.find("firefox"), nil, laptopScreen, hs.layout.maximized, nil, nil},
+      -- {hs.application.find("firefox"), nil, laptopScreen, hs.layout.maximized, nil, nil},
 }
 
 
@@ -113,8 +116,22 @@ hs.hotkey.bind(mash_app, "1", function()
 end)
 hs.hotkey.bind(mash_app, "2", function() hs.application.launchOrFocus("Google Chrome") end)
 hs.hotkey.bind(mash_app, "3", function() hs.application.launchOrFocusByBundleID(FirefoxBrowser) end)
-hs.hotkey.bind(mash_app, "4", function() hs.application.launchOrFocus("Spotify") end)
-hs.hotkey.bind(mash_app, "5", function() hs.application.launchOrFocus("Slack") end)
+hs.hotkey.bind(mash_app, "4", function() 
+  music_app = hs.application.find("YT Music")
+  if (music_app:isFrontmost() == true) then
+    hs.application.launchOrFocus("spotify")
+  else
+    hs.application.launchOrFocus("YT Music")
+  end
+end)
+hs.hotkey.bind(mash_app, "5", function() 
+  comm_app = hs.application.find("Slack")
+  if (comm_app:isFrontmost() == true) then
+    hs.application.launchOrFocus("Signal")
+  else
+    hs.application.launchOrFocus("Slack")
+  end
+end)
 hs.hotkey.bind(mash_app, "6", function() hs.application.launchOrFocus("zoom.us") end)
 hs.hotkey.bind(mash_app, "7", function() 
   hs.application.launchOrFocus("TextEdit")
@@ -123,6 +140,22 @@ hs.hotkey.bind(mash_app, "7", function()
 end)
 hs.hotkey.bind(mash_app, "8", function() hs.application.launchOrFocus("Calculator") end)
 hs.hotkey.bind(mash_app, "9", function() hs.application.launchOrFocus("Evernote") end)
+hs.hotkey.bind(mash_app, "0", function() hs.application.launchOrFocus("Discord") end)
+hs.hotkey.bind(mash_app, "b", function() 
+  output, status, t, rc = hs.execute("/usr/local/bin/blueutil -p")
+  -- foo = hs.logger.new('test', 'debug')
+  -- foo.d(output)
+  output = tonumber(output)
+  if (output == 1) then
+    -- foo.d('here on')
+    hs.execute("/usr/local/bin/blueutil -p 0")
+    hs.alert.show("Bluetooth off");
+  else
+    -- foo.d('here off')
+    hs.execute("/usr/local/bin/blueutil -p 1")
+    hs.alert.show("Bluetooth on");
+  end
+end)
 hs.hotkey.bind(mash_app, "t", function()
   resizewindowz("righthalf")
 end)
@@ -168,6 +201,17 @@ hs.hotkey.bind(mash_app, "c", function()
   f.h = max.h / 2
   win:setFrame(f)
 end)
+hs.hotkey.bind(mash_app, "z", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
 hs.hotkey.bind(mash_app, "n", function()
   local win = hs.window.focusedWindow()
   local screen = win:screen()
@@ -205,6 +249,21 @@ end
 appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
 
+--[[
+function sleepWatch(eventType)
+  local action = "unknown"
+  foo = hs.logger.new('test', 'debug')
+  if (eventType == hs.caffeinate.watcher.systemWillSleep) then
+    hs.execute("/usr/local/bin/blueutil -p 0")
+    foo.d('Bluetooth off')
+  elseif (eventType == hs.caffeinate.watcher.systemDidWake) then
+    hs.execute("/usr/local/bin/blueutil -p 1")
+    foo.d('Bluetooth on')
+  end
+end
+local sleepWatcher = hs.caffeinate.watcher.new(sleepWatch)
+sleepWatcher:start()
+--]]
 
 function reloadConfig(files)
     doReload = false
